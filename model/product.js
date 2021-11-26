330c85c7e52c2490701e4ccd9dff0504e61d6c1f
dung1202 <phamtiendung12022001@gmail.com>
2021-11-26 22:35:28 +0700

    update

--- model/product.js
+++ model/product.js
@@ -1,36 +1,48 @@
 var mongoose = require("mongoose");
 
 var userSchema = mongoose.Schema({
-    userId: {
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: String,
+  listedPrice: Number,
+  discountPrice: Number,
+  is_hot: Boolean,
+  in_slider: Boolean,
+  img: String,
+  listphotos: [
+    {
+      type: String,
+    },
+  ],
+  vote: [
+    {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "User",
+      },
+      number: Number,
+      content: String
     },
-    name: String,
-    listedPrice: Number,
-    discountPrice: Number,
-    is_hot: Boolean,
-    in_slider: Boolean,
-    img: String,
-    listphotos: [{
-        type: String
-    }],
-    quantity: Number,
-    sold: Number,
-    description: String,
-    tags: [{
-        type: String
-    }],
-    supplier: String,
-    createdAt:
+  ],
+  quantity: Number,
+  sold: Number,
+  description: String,
+  tags: [
     {
-        type: Date,
-        default: Date.now(+new Date() + 7 * 60 * 60 * 1000)
+      type: String,
     },
-    updateAt:
-    {
-        type: Date,
-        default: Date.now(+new Date() + 7 * 60 * 60 * 1000)
-    }
+  ],
+  supplier: String,
+  createdAt: {
+    type: Date,
+    default: Date.now(+new Date() + 7 * 60 * 60 * 1000),
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now(+new Date() + 7 * 60 * 60 * 1000),
+  },
 });
 
 var Product = mongoose.model("Product", userSchema);
